c564e75fae04396fadf81bfc0d36b18a9e390cd9
nilkli <nils-ake.klittby@ltu.se>
2020-12-06 20:22:03 +0100

    Mirror User

---
+++ Api/Startup.cs
@@ -0,0 +1,16 @@
+﻿using BlazorApp.Api;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace BlazorApp.Api
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<NyaSenderRestClient>();
+        }
+
+     }
+}
