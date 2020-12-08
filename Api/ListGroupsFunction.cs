1e92f9c2cc5a8a83a466270a0489194c42c3de2b
nilkli <Nils-ke Klittby nilkli@ltu.se>
2020-12-08 12:37:28 +0100

    x

--- Api/ListGroupsFunction.cs
+++ Api/ListGroupsFunction.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
+
 namespace BlazorApp.Api
 {
     public class ListGroupsFunction
